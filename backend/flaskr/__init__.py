9dcd72522cd7ce00c5b271972cac3fc99fb25690
Eud15 <aboutaeudoxie@gmail.com>
2022-09-03 16:37:39 +0100

    Second project of Udacity nanodegree Trivia_api

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,12 +1,23 @@
 import os
+from unicodedata import category
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-from models import setup_db, Question, Category
+from models import setup_db, Question, Category, db
 
 QUESTIONS_PER_PAGE = 10
+
+def paginate_questions(request, selection):
+    page = request.args.get('page', 1, type=int)
+    start = (page - 1) * QUESTIONS_PER_PAGE
+    end = start + QUESTIONS_PER_PAGE
+
+    questions = [question.format() for question in selection]
+    current_questions = questions[start:end]
+
+    return current_questions
 
 def create_app(test_config=None):
     # create and configure the app
@@ -16,17 +27,34 @@ def create_app(test_config=None):
     """
     @TODO: Set up CORS. Allow '*' for origins. Delete the sample route after completing the TODOs
     """
-
+    CORS(app)
+    cors=CORS(app, ressources={r"/api/*": {"origins": "*"}})
     """
     @TODO: Use the after_request decorator to set Access-Control-Allow
     """
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        return response
 
     """
     @TODO:
     Create an endpoint to handle GET requests
     for all available categories.
     """
+    @app.route('/categories')
+    def get_categories():
+        categories = db.session.query(Category).order_by(Category.id).all()
 
+        if len(categories) == 0:
+            abort(404)
+
+        return jsonify({
+            'success': True,
+            'categories' : {category.id: category.type for category in categories}
+        }) 
+        
 
     """
     @TODO:
@@ -40,6 +68,22 @@ def create_app(test_config=None):
     ten questions per page and pagination at the bottom of the screen for three pages.
     Clicking on the page numbers should update the questions.
     """
+    @app.route('/questions')
+    def get_questions():
+        selection = Question.query.order_by(Category.type).all()
+        current_questions = paginate_questions(request, selection)
+        categories = Category.query.order_by(Category.type).all()
+
+        if len(current_questions) == 0:
+            abort(404)
+        return jsonify({
+            'success' : True,
+            'questions' : current_questions,
+            'total_questions' : len(selection),
+            'categories' : {category.id: category.type for category in categories},
+            'current_category':None
+        })
+        
 
     """
     @TODO:
@@ -48,7 +92,18 @@ def create_app(test_config=None):
     TEST: When you click the trash icon next to a question, the question will be removed.
     This removal will persist in the database and when you refresh the page.
     """
-
+    @app.route("/questions/<question_id", methods=['DELETE'])
+    def delete_questions(question_id):
+        try:
+            question = Question.querry.get(question_id)
+            question.delete()
+            return jsonify({
+                'success' : True,
+                'deleted' : question_id
+            })
+        except:
+            abort(422)
+        
     """
     @TODO:
     Create an endpoint to POST a new question,
@@ -59,6 +114,30 @@ def create_app(test_config=None):
     the form will clear and the question will appear at the end of the last page
     of the questions list in the "List" tab.
     """
+    @app.route("/questions", methods=['POST'])
+    def add_question():
+        body = request.get_json()
+
+        if not ('question' in body and 'answer' in body and 'difficulty' in body and 'category' in body):
+            abort(422)
+
+        new_question = body.get('question')
+        new_answer = body.get('answer')
+        new_difficulty = body.get('difficulty')
+        new_category = body.get('category')
+
+        try:
+            question = Question(question=new_question, answer=new_answer,
+                                difficulty=new_difficulty, category=new_category)
+            question.insert()
+
+            return jsonify({
+                'success': True,
+                'created': question.id,
+            })
+
+        except:
+            abort(422)
 
     """
     @TODO:
@@ -70,7 +149,22 @@ def create_app(test_config=None):
     only question that include that string within their question.
     Try using the word "title" to start.
     """
+    @app.route('/questions/search', methods=['POST'])
+    def search_questions():
+        body = request.get_json()
+        search_term = body.get('searchTerm', None)
 
+        if search_term:
+            search_results = Question.query.filter(
+                Question.question.ilike(f'%{search_term}%')).all()
+
+            return jsonify({
+                'success': True,
+                'questions': [question.format() for question in search_results],
+                'total_questions': len(search_results),
+                'current_category': None
+            })
+        abort(404) 
     """
     @TODO:
     Create a GET endpoint to get questions based on category.
@@ -79,6 +173,21 @@ def create_app(test_config=None):
     categories in the left column will cause only questions of that
     category to be shown.
     """
+    @app.route('/categories/<int:category_id>/questions', methods=['GET'])
+    def retrieve_questions_by_category(category_id):
+
+        try:
+            questions = Question.query.filter(
+                Question.category == str(category_id)).all()
+
+            return jsonify({
+                'success': True,
+                'questions': [question.format() for question in questions],
+                'total_questions': len(questions),
+                'current_category': category_id
+            })
+        except:
+            abort(404)
 
     """
     @TODO:
@@ -91,12 +200,65 @@ def create_app(test_config=None):
     one question at a time is displayed, the user is allowed to answer
     and shown whether they were correct or not.
     """
+    @app.route('/quizzes', methods=['POST'])
+    def play_quiz():
+
+        try:
+
+            body = request.get_json()
+
+            if not ('quiz_category' in body and 'previous_questions' in body):
+                abort(422)
+
+            category = body.get('quiz_category')
+            previous_questions = body.get('previous_questions')
+
+            if category['type'] == 'click':
+                available_questions = Question.query.filter(
+                    Question.id.notin_((previous_questions))).all()
+            else:
+                available_questions = Question.query.filter_by(
+                    category=category['id']).filter(Question.id.notin_((previous_questions))).all()
+
+            new_question = available_questions[random.randrange(
+                0, len(available_questions))].format() if len(available_questions) > 0 else None
+
+            return jsonify({
+                'success': True,
+                'question': new_question
+            })
+        except:
+            abort(422)
+
 
     """
     @TODO:
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    @app.errorhandler(404)
+    def not_found(error):
+        return jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+        }), 404
+
+    @app.errorhandler(422)
+    def unprocessable(error):
+        return jsonify({
+            "success": False,
+            "error": 422,
+            "message": "unprocessable"
+        }), 422
+
+    @app.errorhandler(400)
+    def bad_request(error):
+        return jsonify({
+            "success": False,
+            "error": 400,
+            "message": "bad request"
+        }), 400
 
     return app
 
--- frontend/src/components/QuestionView.js
+++ frontend/src/components/QuestionView.js
@@ -1,130 +1,131 @@
-import React, { Component } from 'react';
-import '../stylesheets/App.css';
-import Question from './Question';
-import Search from './Search';
-import $ from 'jquery';
+import React, { Component } from 'react'
+
+import '../stylesheets/App.css'
+import Question from './Question'
+import Search from './Search'
+import $ from 'jquery'
 
 class QuestionView extends Component {
   constructor() {
-    super();
+    super()
     this.state = {
       questions: [],
       page: 1,
       totalQuestions: 0,
       categories: {},
-      currentCategory: null,
-    };
+      currentCategory: null
+    }
   }
 
   componentDidMount() {
-    this.getQuestions();
+    this.getQuestions()
   }
 
   getQuestions = () => {
     $.ajax({
       url: `/questions?page=${this.state.page}`, //TODO: update request URL
       type: 'GET',
-      success: (result) => {
+      success: result => {
         this.setState({
           questions: result.questions,
           totalQuestions: result.total_questions,
           categories: result.categories,
-          currentCategory: result.current_category,
-        });
-        return;
+          currentCategory: result.current_category
+        })
+        return
       },
-      error: (error) => {
-        alert('Unable to load questions. Please try your request again');
-        return;
-      },
-    });
-  };
+      error: error => {
+        alert('Unable to load questions. Please try your request again')
+        return
+      }
+    })
+  }
 
   selectPage(num) {
-    this.setState({ page: num }, () => this.getQuestions());
+    this.setState({ page: num }, () => this.getQuestions())
   }
 
   createPagination() {
-    let pageNumbers = [];
-    let maxPage = Math.ceil(this.state.totalQuestions / 10);
+    let pageNumbers = []
+    let maxPage = Math.ceil(this.state.totalQuestions / 10)
     for (let i = 1; i <= maxPage; i++) {
       pageNumbers.push(
         <span
           key={i}
           className={`page-num ${i === this.state.page ? 'active' : ''}`}
           onClick={() => {
-            this.selectPage(i);
+            this.selectPage(i)
           }}
         >
           {i}
         </span>
-      );
+      )
     }
-    return pageNumbers;
+    return pageNumbers
   }
 
-  getByCategory = (id) => {
+  getByCategory = id => {
     $.ajax({
       url: `/categories/${id}/questions`, //TODO: update request URL
       type: 'GET',
-      success: (result) => {
+      success: result => {
         this.setState({
           questions: result.questions,
           totalQuestions: result.total_questions,
-          currentCategory: result.current_category,
-        });
-        return;
+          currentCategory: result.current_category
+        })
+        return
       },
-      error: (error) => {
-        alert('Unable to load questions. Please try your request again');
-        return;
-      },
-    });
-  };
+      error: error => {
+        alert('Unable to load questions. Please try your request again')
+        return
+      }
+    })
+  }
 
-  submitSearch = (searchTerm) => {
+  submitSearch = searchTerm => {
     $.ajax({
-      url: `/questions`, //TODO: update request URL
+      url: `/questions/search`, //TODO: update request URL
       type: 'POST',
       dataType: 'json',
       contentType: 'application/json',
       data: JSON.stringify({ searchTerm: searchTerm }),
       xhrFields: {
-        withCredentials: true,
+        withCredentials: true
       },
       crossDomain: true,
-      success: (result) => {
+      success: result => {
         this.setState({
           questions: result.questions,
           totalQuestions: result.total_questions,
-          currentCategory: result.current_category,
-        });
-        return;
+          currentCategory: result.current_category
+        })
+        return
       },
-      error: (error) => {
-        alert('Unable to load questions. Please try your request again');
-        return;
-      },
-    });
-  };
+      error: error => {
+        alert('Unable to load questions. Please try your request again')
+        return
+      }
+    })
+  }
 
-  questionAction = (id) => (action) => {
+  questionAction = id => action => {
     if (action === 'DELETE') {
       if (window.confirm('are you sure you want to delete the question?')) {
         $.ajax({
           url: `/questions/${id}`, //TODO: update request URL
           type: 'DELETE',
-          success: (result) => {
-            this.getQuestions();
+          success: result => {
+            this.getQuestions()
           },
-          error: (error) => {
-            alert('Unable to load questions. Please try your request again');
-            return;
-          },
-        });
+          error: error => {
+            alert('Unable to load questions. Please try your request again')
+            return
+          }
+        })
       }
     }
-  };
+  }
 
   render() {
     return (
@@ -132,24 +133,23 @@ class QuestionView extends Component {
         <div className='categories-list'>
           <h2
             onClick={() => {
-              this.getQuestions();
+              this.getQuestions()
             }}
           >
             Categories
           </h2>
           <ul>
-            {Object.keys(this.state.categories).map((id) => (
+            {Object.keys(this.state.categories).map(id => (
               <li
                 key={id}
                 onClick={() => {
-                  this.getByCategory(id);
+                  this.getByCategory(id)
                 }}
               >
                 {this.state.categories[id]}
                 <img
                   className='category'
-                  alt={`${this.state.categories[id].toLowerCase()}`}
-                  src={`${this.state.categories[id].toLowerCase()}.svg`}
+                  src={`${this.state.categories[id]}.svg`}
                 />
               </li>
             ))}
@@ -171,8 +171,8 @@ class QuestionView extends Component {
           <div className='pagination-menu'>{this.createPagination()}</div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default QuestionView;
+export default QuestionView
